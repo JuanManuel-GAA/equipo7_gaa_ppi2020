278eb52fd638a3e37b8fb03c84ea591382d3e3e4
SamuelSanchezGAA <drmanhattan0321@gmail.com>
2020-10-25 19:02:47 -0500

    Pantallas_SSA

---
+++ frontend/src/components/IniciarFacebook.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "../style/IniciarFacebook.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Menu1 } from "./Menu1";
+import { withRouter } from "react-router-dom";
+
+const IniciarFacebook = (props) => {
+  const { history } = props;
+  return (
+    <div>
+      <Menu1 />
+      <div className="ContinuarFacebook">
+        <div className="IconFacebook2 mt-2">
+          <img
+            className="IconFacebook2 mt-1"
+            src="./imagenes_PPI/IconFacebook2.png"
+            alt="Facebook logo"
+          />
+        </div>
+        <div className="text-center">
+          <h5 className="mb-3 mt-3">Continuar como Juan Morales</h5>
+          <div className="aviso1 mt-5">
+            <h6 className="gris">
+              <p>
+                Quality Travel recibirá <br /> su perfil público
+              </p>
+            </h6>
+          </div>
+
+          <div>
+            <h6 className="negro">
+              <FontAwesomeIcon icon={["fas", "edit"]} /> Leer más
+            </h6>
+          </div>
+        </div>
+
+        <div>
+          <div className="btn">
+            <button
+              type="button"
+              className="btn mt-4"
+              onClick={() => history.push("/Inicio2")}
+            >
+              Continuar
+            </button>
+          </div>
+
+          <br />
+          <br />
+
+          <div className="negro2">
+            <p>
+              <FontAwesomeIcon icon={["fas", "lock"]} /> Esto no permite que la
+              aplicación <br /> publique en Facebook
+            </p>
+          </div>
+
+          <br />
+          <br />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(IniciarFacebook);
