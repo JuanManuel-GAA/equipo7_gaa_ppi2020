f8fbf9101577cc30d3aa61822ced5674a26032a2
Camilo Marin Naranjo <66567305+camilomarinN@users.noreply.github.com>
2020-11-14 10:40:15 -0500

    tipo_usuario

--- backend/src/routes/tipo_usuario.js
+++ backend/src/routes/tipo_usuario.js
@@ -9,7 +9,7 @@ router.get('/',(req,res)=>{
 
         // tipo_usuario
     //Petición get
-router.get('/tipo_Usuario',(req,res)=>{
+router.get('/tipo_usuario',(req,res)=>{
     mysqlConnection.query('SELECT * FROM tipo_usuario',
     (err,rows,fields)=>{
       if(!err)
@@ -19,12 +19,11 @@ router.get('/tipo_Usuario',(req,res)=>{
        console.log(err);
      }
     })
-}) 
-
-    //Petición post
+})
+//Petición post
    router.post('/tipo_usuario', (req, res) => {
-    const {id,nombre,descripcion} = req.body
-    let tipo_usuario = [id,nombre,descripcion];
+    const {ID,nombre,descripcion} = req.body
+    let tipo_usuario = [ID,nombre,descripcion];
     let nuevoUsuario = `INSERT INTO tipo_usuario VALUES (?,?,?);`
 
    mysqlConnection.query(nuevoUsuario,tipo_usuario, (err,results,fields) => {
@@ -34,16 +33,15 @@ router.get('/tipo_Usuario',(req,res)=>{
      res.json({message:`Usuario ingresado`})
    });
   });
-  
-     //Petición put
-  router.put('/tipo_usuario/:id', (req,res) => {
+/Petición put
+  router.put('/tipo_usuario/:ID', (req,res) => {
   const {nombre, descripcion} = req.body
-  const { id } = req.params 
+  const { ID } = req.params 
 
 mysqlConnection.query(`UPDATE tipo_usuario
                        SET nombre=?, descripcion=?
-                       WHERE id = ?`,
-                       [nombre,descripcion,id], (err, rows,fields) => {
+                       WHERE ID = ?`,
+                       [nombre,descripcion,ID], (err, rows,fields) => {
    if(!err){
     res.json({status: `Tipo de usuario actualizado con éxito`});
    }else{
@@ -53,9 +51,9 @@ mysqlConnection.query(`UPDATE tipo_usuario
 });
 
   //PETICIÓN O SERVICIO DELETE - ELIMINACIÓN DE DATOS
-  router.delete('/tipo_usuario/:id', (req,res) => {
-    const { id } = req.params;
-    mysqlConnection.query(`DELETE FROM tipo_usuario WHERE id =?`,[id],(err,rows,fields) => {
+  router.delete('/tipo_usuario/:ID', (req,res) => {
+    const { ID } = req.params;
+    mysqlConnection.query(`DELETE FROM tipo_usuario WHERE ID =?`,[ID],(err,rows,fields) => {
       if("!err"){
         res.json({status: `Tipo de usuario eliminado con éxito`})
       }else{
