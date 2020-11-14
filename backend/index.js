02a8cfc85cf0de7050894ca51b001ab431095c8c
Samuel Sánchez Álvarez <66803457+SamuelSanchezGAA@users.noreply.github.com>
2020-11-14 11:06:52 -0500

    Tabla parques_guardados XD

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,12 @@ const parques_visitados = require ('./src/routes/parques_visitados.js');
 const tipo_usuario = require ('./src/routes/tipo_usuario.js');
 const visita = require ('./src/routes/visita.js');
 const usuario = require ('./src/routes/usuario.js');
+const parques_guardados = require ('./src/routes/parques_guardados');
 
 app.use('/api',parque);
 app.use('/api',parques_visitados);
 app.use('/api',tipo_usuario);
+app.use('/api',parques_guardados);
 app.use('/api',visita);
 app.use('/api',usuario);
 app.use(express.urlencoded({extended: false}));
