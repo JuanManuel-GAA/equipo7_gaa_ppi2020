672e227c52822c4cbb526234a2c344fbc5fd1a03
Juan Manuel Morales <66565562+JuanManuel-GAA@users.noreply.github.com>
2020-11-19 13:20:27 -0500

    Cambios

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,22 @@
 const express = require ('express');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 
 const parque = require ('./src/routes/parque.js');
-const parques_visitados = require ('./src/routes/parques_visitados.js');
+const parque_calificado = require ('./src/routes/parque_calificado.js');
+const parque_visitado = require ('./src/routes/parque_visitado.js');
 const tipo_usuario = require ('./src/routes/tipo_usuario.js');
-const visita = require ('./src/routes/visita.js');
 const usuario = require ('./src/routes/usuario.js');
-const parques_guardados = require ('./src/routes/parques_guardados');
+const parque_guardado = require ('./src/routes/parque_guardado.js');
 
 app.use('/api',parque);
+app.use('/api',parque_visitado);
+app.use('/api',parque_calificado);
 app.use('/api',tipo_usuario);
+app.use('/api',usuario);
+app.use('/api',parque_guardado);
 
 
 app.use(express.urlencoded({extended: false}));
