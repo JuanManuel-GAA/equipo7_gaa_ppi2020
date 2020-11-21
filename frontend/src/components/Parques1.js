1b3ecd080e592f96226cd09f822a00ecdc36d1e7
Samuel Sánchez Álvarez <66803457+SamuelSanchezGAA@users.noreply.github.com>
2020-11-21 10:37:26 -0500

    Modal Calificar XDDD

--- frontend/src/components/Parques1.js
+++ frontend/src/components/Parques1.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Navbar, Form, FormControl } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
 import "../style/Parques1.css";
@@ -76,27 +77,131 @@ const Parques1 = (props) => {
         ></img>
       </div>
       <button
-        className="Boton2 mt-1 btn"
-        onClick={() => history.push("/Calificar")}
+        type="button"
+        class="Boton2 btn-primary"
+        data-toggle="modal"
+        data-target="#Estrellas"
       >
-        {" "}
-        Calificar{" "}
+        Calificar
       </button>
-      <div className="comentario1">
-        <Form>
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1 ">Comentario:</label>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="1"
-            ></textarea>
-            <div className="leer">
-              <Link to="/Comentarios">Leer más</Link>
+
+      <div
+        class="modal fade"
+        id="Estrellas"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
             </div>
-            <label for="exampleFormControlTextarea1"></label>
+            <div class="modal-body">
+              <div className="">
+                <h1 className="com">
+                  Tu valoración es muy importante para nosotros
+                </h1>
+                <div className="container container1">
+                  <div className="post">
+                    <div className="text">¡Gracias por valorarlo!</div>
+                    <div className="edit">BACK</div>
+                  </div>
+                  <div className="star-widget">
+                    <input type="radio" name="rate" id="rate-5" />
+                    <label htmlFor="rate-5">
+                      <FontAwesomeIcon icon={["fas", "star"]} />
+                    </label>
+                    <input type="radio" name="rate" id="rate-4" />
+                    <label htmlFor="rate-4">
+                      <FontAwesomeIcon icon={["fas", "star"]} />
+                    </label>
+                    <input type="radio" name="rate" id="rate-3" />
+                    <label htmlFor="rate-3">
+                      <FontAwesomeIcon icon={["fas", "star"]} />
+                    </label>
+                    <input type="radio" name="rate" id="rate-2" />
+                    <label htmlFor="rate-2">
+                      <FontAwesomeIcon icon={["fas", "star"]} />
+                    </label>
+                    <input type="radio" name="rate" id="rate-1" />
+                    <label htmlFor="rate-1">
+                      <FontAwesomeIcon icon={["fas", "star"]} />
+                    </label>
+
+                    <form action="/">
+                      <header></header>
+                      <div className="textarea">
+                        <textarea
+                          cols="30"
+                          placeholder="Describe tu experiencia.."
+                        ></textarea>
+                      </div>
+                      <div className="btn1">
+                        <button
+                          type="button"
+                          class="BR"
+                          data-toggle="modal"
+                          data-target="#Calificar"
+                        >
+                          PUBLICAR
+                        </button>
+
+                        <div
+                          class="modal fade"
+                          id="Calificar"
+                          tabindex="-1"
+                          role="dialog"
+                          aria-labelledby="exampleModalLabel"
+                          aria-hidden="true"
+                        >
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">
+                                  ¡Muchas gracias!
+                                </h5>
+                                <button
+                                  type="button"
+                                  class="close"
+                                  data-dismiss="Estrellas"
+                                  aria-label="Close"
+                                >
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                              <div class="modal-body">
+                                Tu calificación se ha enviado satisfactoriamente
+                              </div>
+                              <div class="modal-footer">
+                                <button
+                                  type="button"
+                                  class="btn btn-secondary"
+                                  data-dismiss="modal"
+                                  onClick={() => history.push("/Parques1")}
+                                >
+                                  Cerrar
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </Form>
+        </div>
       </div>
     </div>
   );
