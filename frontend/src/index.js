08502916eee1f5b882ddf9a779890c306f6a04bb
Camilo Marin Naranjo <66567305+camilomarinN@users.noreply.github.com>
2020-11-22 14:55:50 -0500

    act_22/11

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import $ from "jquery";
-import parallax from "jquery"; 
 import Popper from "popper.js";
 
 
