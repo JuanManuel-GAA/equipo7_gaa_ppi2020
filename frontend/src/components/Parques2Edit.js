dc55b733cc45a8e112afd51b8db1a13f2c47fe16
Maricela Botero Jaramillo <66274511+Maricelabotero@users.noreply.github.com>
2020-11-26 13:06:55 -0500

    parques2Edit

---
+++ frontend/src/components/Parques2Edit.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+
+import Menu2 from "./Menu2";
+const Parques2Edit = () => {
+  return (
+    <div className="Parques2Edit">
+
+</div>
+  );
+};
+export default withRouter(Parques2Edit);
